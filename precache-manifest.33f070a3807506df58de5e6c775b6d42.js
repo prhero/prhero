e4fe2eb8ebf08605dedd5234065e03bcbdc423d2
Colin Walker <colinwalker270@gmail.com>
2019-12-27 23:40:05 +0000

    Updates

---
+++ precache-manifest.33f070a3807506df58de5e6c775b6d42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73ceb341982d50dc77782cd6189b7202",
+    "url": "/prhero/index.html"
+  },
+  {
+    "revision": "0f352723b66aa3758168",
+    "url": "/prhero/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "3c5213001c24fa65a6e0",
+    "url": "/prhero/static/css/main.587c7ccf.chunk.css"
+  },
+  {
+    "revision": "0f352723b66aa3758168",
+    "url": "/prhero/static/js/2.9243836d.chunk.js"
+  },
+  {
+    "revision": "c5682c5d77e856e2d28652c66e6435c2",
+    "url": "/prhero/static/js/2.9243836d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3c5213001c24fa65a6e0",
+    "url": "/prhero/static/js/main.cf0b0d0b.chunk.js"
+  },
+  {
+    "revision": "9bb355abf8e467b72212",
+    "url": "/prhero/static/js/runtime-main.88e56a9f.js"
+  }
+]);
